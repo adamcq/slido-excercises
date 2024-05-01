9719e6dc017fed5ecaf9f9781d429feaebebab82
borgula <borgula@kth.se>
2024-05-01 10:54:28 +0200

    gtests added

--- cpp-excercise/Average/tests/test.cpp
+++ cpp-excercise/Average/tests/test.cpp
@@ -1,8 +1,39 @@
-// #include "gtest/gtest.h"
-// #include "MathFunctions.h"
+#include "gtest/gtest.h"
+#include "MathFunctions.h"
 
-// TEST(AverageTest, BasicTest) {
-//   std::vector<int> numbers = {1, 2, 3, 4, 5};
-//   double expected = 3.0;
-//   ASSERT_EQ(computeAverage(numbers), expected);
-// }
+TEST(AverageTest, BasicTest) {
+  std::vector<double> numbers = {1, 2, 3, 4, 5};
+  double expected = 3.0;
+  ASSERT_EQ(mathfunctions::average(numbers), expected);
+}
+
+TEST(AverageTest, EmptyTest) {
+  std::vector<double> numbers = {};
+  double expected = 0.0;
+  ASSERT_EQ(mathfunctions::average(numbers), expected);
+}
+
+TEST(AverageTest, NegativeTest) {
+  std::vector<double> numbers = {-1, -2, -3, -4, -5};
+  double expected = -3.0;
+  ASSERT_EQ(mathfunctions::average(numbers), expected);
+}
+
+TEST(AverageTest, ZeroTest) {
+  std::vector<double> numbers = {0, 0, 0, 0, 0};
+  double expected = 0.0;
+  ASSERT_EQ(mathfunctions::average(numbers), expected);
+}
+
+TEST(AverageTest, OneElementTest) {
+  std::vector<double> numbers = {1};
+  double expected = 1.0;
+  ASSERT_EQ(mathfunctions::average(numbers), expected);
+}
+
+TEST(AverageTest, DecimalTest) {
+  std::vector<double> numbers = {1.5, 2.5, 3.5, 4.5, 5.5};
+  double expected = 3.5;
+  ASSERT_EQ(mathfunctions::average(numbers), expected);
+}
+
